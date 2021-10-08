bf66ca7f4bc7741debb224c998369310a6800124
Shadowda <piercej2000@gmail.com>
2021-10-07 23:32:49 -0400

    C# updated with tree structure

--- Reader/Program.cs
+++ Reader/Program.cs
@@ -12,7 +12,76 @@ namespace Reader
         
         // size (number_of_items) dervied: Children.Count
         public int Size { get; set; }
-        public string LastModified { get; set; }
+        public DateTime LastModified { get; set; }
+
+        // Process all files in the directory passed in, recurse on any directories
+        // that are found, and process the files they contain.
+        public FileOrDirectory ProcessDirectory(string targetDirectory, int depth)
+        {
+            if (depth > 1)
+                return null;
+
+            // Process the list of files found in the directory.
+            string[] fileEntries;
+
+            try
+            {
+                fileEntries = System.IO.Directory.GetFiles(targetDirectory);
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                fileEntries = new string[0];
+            }
+
+            var childTree = new List<FileOrDirectory>();
+
+            foreach (string fileName in fileEntries)
+                childTree.Add(ProcessFile(fileName));
+
+            // Recurse into subdirectories of this directory.
+            string[] subdirectoryEntries;
+
+            try
+            {
+                subdirectoryEntries = System.IO.Directory.GetDirectories(targetDirectory);
+            }
+            catch (System.UnauthorizedAccessException)
+            {
+                subdirectoryEntries = new string[0];
+            }
+
+            foreach (string subdirectory in subdirectoryEntries)
+                childTree.Add(ProcessDirectory(subdirectory, depth + 1));
+
+            System.IO.DirectoryInfo dirInfo = new System.IO.DirectoryInfo(targetDirectory);
+            FileOrDirectory d = new FileOrDirectory();
+            d.Path = targetDirectory;
+            d.Name = dirInfo.Name;
+            d.Size = childTree.Count;
+            d.LastModified = dirInfo.LastWriteTime;
+            d.Children = childTree;
+
+
+            Console.WriteLine("Processed dir '{0}'.\n", targetDirectory);
+            //System.IO.DirectoryInfo dirInfo = new System.IO.DirectoryInfo(targetDirectory);
+            //Console.WriteLine(dirInfo.LastWriteTime);
+            return d;
+        }
+
+        // Insert logic for processing found files here.
+        public FileOrDirectory ProcessFile(string path)
+        {
+            var fInfo = new System.IO.FileInfo(path);
+
+            FileOrDirectory f = new FileOrDirectory();
+            f.Path = path;
+            f.Name = fInfo.Name;
+            f.Size = 0;
+            f.LastModified = fInfo.LastWriteTime;
+            f.Children = null;
+
+            return f;
+        }
     }
 
     class Program
@@ -24,7 +93,14 @@ namespace Reader
             Console.WriteLine("GetLogicalDrives: {0}", String.Join(", ", drives));
 
             //RecursiveFileProcessor
-            ProcessDirectory(drives[0], 0);
+            FileOrDirectory root = new FileOrDirectory();
+            root = root.ProcessDirectory(drives[0], 0);
+
+            Console.WriteLine(root.Path);
+            Console.WriteLine(root.Name);
+            Console.WriteLine(root.Size);
+            Console.WriteLine(root.Children[0].Path);
+            Console.WriteLine(root.Children[0].Name);
 
             { 
             /*
@@ -51,61 +127,6 @@ namespace Reader
                 Console.WriteLine(d.Name);
             }
             */}
-        }
-
-        // Process all files in the directory passed in, recurse on any directories
-        // that are found, and process the files they contain.
-        public static void ProcessDirectory(string targetDirectory, int depth)
-        {
-            if (depth > 1)
-                return;
-
-            FileOrDirectory d = new FileOrDirectory();
-            d.Path = targetDirectory;
-            d.name = 
-
-
-            // Process the list of files found in the directory.
-            string[] fileEntries;
-
-            try
-            {
-                fileEntries = System.IO.Directory.GetFiles(targetDirectory);
-            }
-            catch (System.UnauthorizedAccessException)
-            {
-                fileEntries = new string[0];
-            }
-
-
-            foreach (string fileName in fileEntries)
-                ProcessFile(fileName);
-
-            // Recurse into subdirectories of this directory.
-            string[] subdirectoryEntries;
-
-            try
-            {
-                subdirectoryEntries = System.IO.Directory.GetDirectories(targetDirectory);
-            }
-            catch (System.UnauthorizedAccessException)
-            {
-                subdirectoryEntries = new string[0];
-            }
-
-            foreach (string subdirectory in subdirectoryEntries)
-                ProcessDirectory(subdirectory, depth + 1);
-
-            Console.WriteLine("Processed dir '{0}'.\n", targetDirectory);
-            System.IO.DirectoryInfo dirInfo = new System.IO.DirectoryInfo(targetDirectory);
-            Console.WriteLine(dirInfo.LastWriteTime);
-
-        }
-
-        // Insert logic for processing found files here.
-        public static void ProcessFile(string path)
-        {
-            Console.WriteLine("Processed file '{0}'.", path);
         }
     }
 }
