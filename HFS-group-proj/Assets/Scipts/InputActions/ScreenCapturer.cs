3089cb74ce1682e40a9a08366b657ac9e2730927
Connor Markwell <markwell.connor@gmail.com>
2021-10-14 00:39:14 -0400

    Rename classes, Restructure GameObject components

--- HFS-group-proj/Assets/Scipts/InputActions/ScreenCapturer.cs
+++ HFS-group-proj/Assets/Scipts/InputActions/ScreenCapturer.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PrimaryReactor : MonoBehaviour
+public class ScreenCapturer : MonoBehaviour
 {
     public PrimaryButtonWatcher watcher;
 
